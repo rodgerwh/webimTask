c750442d6f1de58c839c264785e13e7b590c4447
rodgerwh <plumphelmetson@gmail.com>
2024-05-09 17:48:48 +0300

    env fix for koyeb

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+import os
 from functools import lru_cache
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -11,12 +12,10 @@ class Settings(BaseSettings):
 
     # GitHub OAuth
     GITHUB_CLIENT_ID: str = "Iv23ligsAaz9odF6zv04"
-    GITHUB_CLIENT_SECRET: str
+    GITHUB_CLIENT_SECRET: str = os.environ.get("GITHUB_CLIENT_SECRET")
     GITHUB_AUTHORIZE_URL: str = "https://github.com/login/oauth/authorize"
     GITHUB_TOKEN_URL: str = "https://github.com/login/oauth/access_token"
     GITHUB_USER_URL: str = "https://api.github.com/user"
-
-    model_config = SettingsConfigDict(env_file=".env")
 
 
 @lru_cache
